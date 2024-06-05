a0a1b24cfd222db55381d748d3cfff68985800a2
ryansatriia <ryan.satria100@gmail.com>
2024-06-05 23:36:32 +0700

    basic design for main page

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.carousel-container').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+    });
+});
