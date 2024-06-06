fb88aee2c09ce21c78d3255ef65619bff443c24d
George535220173 <george.535220173@stu.untar.ac.id>
2024-06-06 18:16:59 +0700

    Putting and setting up the laravel + postgre pgadmin

---
+++ UAS_BACKEND_ARSITEK/resources/views/layouts/layout.blade.php
@@ -0,0 +1,24 @@
+<!-- resources/views/layouts/layout.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/portfolio.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+</head>
+<body>
+    @include('partials.header')
+    @yield('content')
+    @include('partials.footer')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
+    <script src="{{ asset('js/index.js') }}"></script>
+    <script src="{{ asset('js/portfolio.js') }}"></script>
+</body>
+</html>
