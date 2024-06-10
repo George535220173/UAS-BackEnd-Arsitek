54c472f3f0d6eee8106fc9e144afffa75dd5795b
Gawira <149558045+Gawira@users.noreply.github.com>
2024-06-10 22:41:45 +0700

    add asrul

--- UAS_BACKEND_ARSITEK/resources/views/layouts/app.blade.php
+++ UAS_BACKEND_ARSITEK/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel App</title>
 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> <!--Font-->
@@ -28,6 +29,9 @@
     <script src="{{ asset('js/index.js') }}"></script>
     <script src="{{ asset('js/portofolio.js') }}"></script>
     <script src="{{ asset('js/service.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     @stack('scripts')
 </body>
 </html>
