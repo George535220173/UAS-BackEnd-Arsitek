0adfddd053882b9da3a76ef3a90c5d42a54b9640
ryansatriia <ryan.satria100@gmail.com>
2024-06-20 00:48:57 +0700

    article shown in main page

--- UAS_BACKEND_ARSITEK/app/Http/Controllers/HomeController.php
+++ UAS_BACKEND_ARSITEK/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article; // Import the Article model
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $articles = Article::orderBy('created_at', 'desc')->get(); // Fetch all articles
+        return view('index', compact('articles'));
     }
+
 }
