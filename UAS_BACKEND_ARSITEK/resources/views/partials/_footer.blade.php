f1864f26a750c4ae241234f0d597322e1f3e1436
ryansatriia <ryan.satria100@gmail.com>
2024-06-25 12:13:18 +0700

    changed most footer's content

--- UAS_BACKEND_ARSITEK/resources/views/partials/_footer.blade.php
+++ UAS_BACKEND_ARSITEK/resources/views/partials/_footer.blade.php
@@ -1,48 +1,31 @@
 <!-- Footer -->
 <footer class="footer">
     <div class="footer-content">
-        <div class="footer-section">
-            <h2>GP</h2>
-            <p>A108 Adam Street<br>New York, NY 535022</p>
-            <p>Phone: +1 5589 55488 55<br>Email: info@example.com</p>
+        <div class="footer-section logo-section">
+            <h2>MRS Architects</h2>
+        </div>
+        <div class="footer-section links-section">
+            <ul>
+                <li><a href="{{ url('/#') }}">Home</a></li>
+                <li><a href="{{ url('/projects/architecture') }}">Architectures</a></li>
+                <li><a href="{{ url('/projects/interiordesign') }}">Interior Designs</a></li>
+                <li><a href="{{ url('/#article-section') }}">Articles</a></li>
+                <li><a href="{{ url('/team') }}">Our Team</a></li>
+                <li><a href="{{ url('/#contact') }}">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="footer-section social-section">
             <div class="socials">
-                <a href="#" class="social-box"><i class="fab fa-twitter"></i></a>
-                <a href="#" class="social-box"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="social-box"><i class="fab fa-pinterest"></i></a>
                 <a href="#" class="social-box"><i class="fab fa-instagram"></i></a>
                 <a href="#" class="social-box"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#" class="social-box"><i class="fab fa-youtube"></i></a>
+                <a href="#" class="social-box"><i class="fab fa-twitter"></i></a>
             </div>
-        </div>
-        <div class="footer-section">
-            <h2>Useful Links</h2>
-            <ul>
-                <li><a href="{{ url('/') }}">Home</a></li>
-                <li><a href="{{ url('/about') }}">About us</a></li>
-                <li><a href="{{ url('/services') }}">Services</a></li>
-                <li><a href="{{ url('/terms') }}">Terms of service</a></li>
-                <li><a href="{{ url('/privacy') }}">Privacy policy</a></li>
-            </ul>
-        </div>
-        <div class="footer-section">
-            <h2>Our Services</h2>
-            <ul>
-                <li><a href="{{ url('/services/web-design') }}">Web Design</a></li>
-                <li><a href="{{ url('/services/web-development') }}">Web Development</a></li>
-                <li><a href="{{ url('/services/product-management') }}">Product Management</a></li>
-                <li><a href="{{ url('/services/marketing') }}">Marketing</a></li>
-                <li><a href="{{ url('/services/graphic-design') }}">Graphic Design</a></li>
-            </ul>
-        </div>
-        <div class="footer-section">
-            <h2>Our Newsletter</h2>
-            <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-            <form class="newsletter">
-                <input type="email" placeholder="Enter your email address">
-                <button type="submit">Subscribe</button>
-            </form>
         </div>
     </div>
     <div class="footer-bottom">
-        <p>&copy; Copyright GP. All Rights Reserved</p>
-        <p>Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a></p>
+        <p>MRS Architects Company Profile Website</p>
+        <p>Created by: George Weilis Junior [535220173], Ryan Satria [535220190], Tiga Wira Prasidya [535220227]</p>
     </div>
 </footer>
