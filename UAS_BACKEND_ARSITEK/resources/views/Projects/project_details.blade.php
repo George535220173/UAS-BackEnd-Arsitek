f1e420eef9af7416567a99ae8521a2bc96a156fa
George535220173 <george.535220173@stu.untar.ac.id>
2024-06-27 21:36:28 +0700

    fix bug

--- UAS_BACKEND_ARSITEK/resources/views/Projects/project_details.blade.php
+++ UAS_BACKEND_ARSITEK/resources/views/Projects/project_details.blade.php
@@ -5,31 +5,26 @@
     <h1>{{ $project->project_name }}</h1>
     <div class="project-inside-container">
         @if($project->images->isNotEmpty())
-            <div id="projectCarousel" class="carousel slide project-details-carousel" data-ride="carousel"
-                data-interval="5000">
+            <div id="projectCarousel" class="carousel slide project-details-carousel" data-ride="carousel" data-interval="5000">
                 <!-- Indicators -->
                 <ol class="carousel-indicators project-details-carousel-indicators">
                     @foreach($project->images as $key => $image)
-                        <li data-target="#projectCarousel" data-slide-to="{{ $key }}" class="{{ $key == 0 ? 'active' : '' }}">
-                        </li>
+                        <li data-target="#projectCarousel" data-slide-to="{{ $key }}" class="{{ $key == 0 ? 'active' : '' }}"></li>
                     @endforeach
                 </ol>
 
                 <div class="carousel-inner project-details-carousel-inner">
                     @foreach($project->images as $key => $image)
                         <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                            <img src="{{ asset('img/Project/' . basename($image->path)) }}"
-                                class="d-block w-100 project-details-carousel-image" alt="Project Image">
+                            <img src="{{ asset('img/Project/' . basename($image->path)) }}" class="d-block w-100 project-details-carousel-image" alt="Project Image">
                         </div>
                     @endforeach
                 </div>
-                <a class="carousel-control-prev project-details-carousel-control-prev" href="#projectCarousel" role="button"
-                    data-slide="prev">
+                <a class="carousel-control-prev project-details-carousel-control-prev" href="#projectCarousel" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
-                <a class="carousel-control-next project-details-carousel-control-next" href="#projectCarousel" role="button"
-                    data-slide="next">
+                <a class="carousel-control-next project-details-carousel-control-next" href="#projectCarousel" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                 </a>
